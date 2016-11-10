f7a882ce518951e8cb756c3e7d6d39f358015dd3
Matt Beard <mattb@data-8.co.uk>
2016-11-10 11:22:33 +0000

    Sample JS added

---
+++ DynamicsBookmarklets/JS/Navigation/Open Advanced Find.js
@@ -0,0 +1,1 @@
+(function () { var form = $("iframe").filter(function () { return $(this).css("visibility") == "visible" })[0].contentWindow; window.open(form.Xrm.Page.context.getClientUrl() + "/main.aspx?pagetype=advancedfind"); })();
---
+++ DynamicsBookmarklets/JS/Records/Activate Record.js
@@ -0,0 +1,1 @@
+(function () { var form = $("iframe").filter(function () { return $(this).css("visibility") == "visible" })[0].contentWindow; form.Mscrm.CommandBarActions.activate(form.Xrm.Page.data.entity.getId(), form.Xrm.Page.data.entity.getEntityName()); })();
